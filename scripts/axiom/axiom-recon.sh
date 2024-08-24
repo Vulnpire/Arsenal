12585907bef808baff8efd9b1d52ddb4b58fdc12
Vulnpire <89225495+Vulnpire@users.noreply.github.com>
2024-08-24 10:01:16 +0200

    axiom-recon.sh

--- scripts/axiom/axiom-recon.sh
+++ scripts/axiom/axiom-recon.sh
@@ -54,7 +54,7 @@ while [[ "$#" -gt 0 ]]; do
             shift
             ;;
         *)
-            echo "Usage: $0 [-f file] [-subs] [-ip] [-crawl] [-waymore] [-check] [-all] [-mass-dns]"
+            echo "Usage: $0 [-f file] [-subs] [-ip] [-crawl] [-waymore] [-check] [-all] [-mass-dns] [-domain]"
             exit 1
             ;;
     esac
@@ -64,7 +64,7 @@ run_subdomain_enumeration() {
     axiom-scan "$FILE" -m subfinder -all -silent -recursive --rm-logs -anew sub.txt
     axiom-scan "$FILE" -m assetfinder -subs-only --rm-logs -anew sub.txt
     shosubgo -f "$FILE" -s "$SHODAN_API_KEY" | anew sub.txt
-    axiom-scan "$FILE" -m findomain --external-subdomains -r -anew temp && mv temp sub.txt
+    axiom-scan "$FILE" -m findomain --external-subdomains -r -anew temp --rm-logs && cat temp | anew sub.txt
     cat sub.txt | sort -u > temp && mv temp sub.txt
     run_probing
 }
@@ -83,7 +83,7 @@ run_probing() {
     axiom-exec "curl -s https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt > /home/op/lists/resolvers.txt"
     axiom-scan sub.txt -m dnsx -threads 300 -o dnsx.txt --rm-logs
     axiom-scan dnsx.txt -m httpx -threads 300 -rl 250 -random-agent -title -td -probe -sc -ct -server -anew techs.txt --rm-logs
-    cat techs.txt | grep -v "FAILED" | awk '{print $1}' > subdomains/alive.txt && mv techs.txt subdomains/
+    mv techs.txt subdomains/
     rm sub.txt
 }
 
@@ -97,13 +97,13 @@ run_sub_portscan() {
 }
 
 run_waymore() {
-    timeout --foreground 6700 axiom-scan "$FILE" -m waymore -p 4 -mc 200 -mode U --rm-logs -anew gau.txt
+    timeout --foreground 6700 axiom-scan "$FILE" -m waymore -p 5 -mc 200 -mode U --rm-logs -anew gau.txt
     run_crawling
 }
 
 run_crawling() {
     timeout --foreground 6700 axiom-scan "$FILE" -m waybackurls --rm-logs -anew gau.txt
-    cat "$FILE" | gau --threads 16 --subs --providers wayback,commoncrawl,otx,urlscan --blacklist png,jpg,jpeg,gif,mp3,mp4,svg,woff,woff2,otf,css,exe,ttf,eot | anew gau.txt
+    cat "$FILE" | gau --threads 25 --subs --providers wayback,commoncrawl,otx,urlscan --mc 200 --blacklist png,jpg,jpeg,gif,mp3,mp4,svg,woff,woff2,otf,css,exe,ttf,eot | anew gau.txt
     grep -Evi "png|jpg|gif|jpeg|swf|woff|svg|pdf|css|webp|woff|woff2|eot|ttf|otf|mp4|txt" gau.txt | sort -u > temp && mv temp gau.txt
     sed 's|^|http://|' "$FILE" > crawl.txt
 
@@ -112,7 +112,7 @@ run_crawling() {
 
 run_advanced_crawling() {
     timeout --foreground 5000 axiom-scan crawl.txt -m gospider --subs --include-subs -o out --rm-logs
-    find plus/ -type f -exec cat {} + | sed -e 's/^\[linkfinder\] - //g' \
+    find out/ -type f -exec cat {} + | sed -e 's/^\[linkfinder\] - //g' \
                                       -e 's/^\[url\] - \[code-[0-9]\{3\}\] - //g' \
                                       -e 's/^\[href\] - //g' \
                                       -e 's/^\[subdomains\] - //g' \
@@ -143,22 +143,36 @@ run_advanced_crawling2() {
 
 run_katana() {
     local headers='-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.171 Safari/537.36"'
-    #local active_headers='-H "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 OPR/110.0.0.0 (Edition Yx 05)"'
-
+ 
     timeout --foreground 3700 axiom-scan crawl.txt -m katana -jsluice -kf all -pss waybackarchive,commoncrawl,alienvault -passive -jc $headers -nc -d 10 -aff -c 30 -silent -s breadth-first -rl 190 -anew vkatana.txt --rm-logs
     cat vkatana.txt | anew hakrawler.txt && rm vkatana.txt
 
-    #timeout --foreground 3700 axiom-scan crawl.txt -m katana $active_headers -jc -d 10 -s breadth-first -jsluice -aff --rm-logs -anew akatana.txt
-    timeout --foreground 3200 axiom-scan hakrawler.txt -m hakrawler -subs -o plus --rm-logs && cat plus | anew akatana.txt && rm plus
-    cat akatana.txt | anew hakrawler.txt && cat hakrawler.txt | sort -u > temp && mv temp hakrawler.txt
+    timeout --foreground 3200 axiom-scan hakrawler.txt -m hakrawler -subs -o plus --rm-logs && cat plus | anew hakrawler.txt && rm plus
+    cat hakrawler.txt | sort -u > temp && mv temp hakrawler.txt
     cat gau.txt hakrawler.txt | sort -u | uro > uri.txt
-    rm crawl.txt gau.txt hakrawler.txt akatana.txt
+    rm crawl.txt gau.txt hakrawler.txt
     mv uri.txt crawl/
+
+    timeout --foreground 3300 axiom-scan crawl/uri.txt -m gospider --subs --include-subs -o plus --rm-logs
+
+    find plus/ -type f -exec cat {} + | sed -e 's/^\[linkfinder\] - //g' \
+                                      -e 's/^\[url\] - \[code-[0-9]\{3\}\] - //g' \
+                                      -e 's/^\[href\] - //g' \
+                                      -e 's/^\[subdomains\] - //g' \
+                                      -e 's/^\[javascript\] - //g' \
+                                      -e 's/^\[form\] - //g' \
+                                      -e 's/^\[upload-form\] - //g' \
+                                      -e 's/^\[aws-s3\] - //g' \
+                                      -e 's/[[:space:]]*$//g' \
+                                      -e '/^$/d' | grep -Evi "png|jpg|gif|jpeg|swf|woff|svg|pdf|css|webp|woff|woff2|eot|ttf|otf|mp4|txt" | sort -u > cleaned_output.txt
+    rm -rf plus/
+    cat cleaned_output.txt | anew crawl/uri.txt && rm cleaned_output.txt
+    grep -E "$(paste -sd '|' wildcards.txt)" crawl/uri.txt > temp && mv temp crawl/uri.txt
+    cat crawl/uri.txt | grep -Ei "\.js$" > crawl/js.urls
 }
 
 run_only_domain() {
-    timeout --foreground 6700 axiom-scan "$FILE" -m waymore -p 4 -mc 200 -mode U --rm-logs -anew gau.txt
-    timeout --foreground 6700 axiom-scan "$FILE" -m waybackurls --rm-logs -anew gau.txt
+    timeout --foreground 6700 axiom-scan "$FILE" -m waymore -n -p 4 -mc 200 -mode U --rm-logs -anew gau.txt
     cat "$FILE" | gau --threads 16 --providers wayback,commoncrawl,otx,urlscan --blacklist png,jpg,jpeg,gif,mp3,mp4,svg,woff,woff2,otf,css,exe,ttf,eot | anew gau.txt
     grep -Evi "png|jpg|gif|jpeg|swf|woff|svg|pdf|css|webp|woff|woff2|eot|ttf|otf|mp4|txt" gau.txt | sort -u > temp && mv temp gau.txt
     sed 's|^|http://|' "$FILE" > crawl.txt
@@ -192,7 +206,9 @@ run_only_domain() {
     cat akatana.txt | anew hakrawler.txt && cat hakrawler.txt | sort -u > temp && mv temp hakrawler.txt
     cat gau.txt hakrawler.txt | sort -u | uro > uri.txt
     rm crawl.txt gau.txt hakrawler.txt akatana.txt
+    grep -E "$(paste -sd '|' wildcards.txt)" uri.txt > temp && mv temp uri.txt
     mv uri.txt crawl/
+    cat crawl/uri.txt | grep -Ei "\.js$" > crawl/js.urls
 
     run_checks
 }
@@ -241,6 +257,7 @@ elif $DOMAIN; then
 else
     echo "Usage: $0 [-f file] [-subs] [-ip] [-crawl] [-waymore] [-check] [-all] [-domain]"
     exit 1
+    
 fi
 
 rm -rf sub.txt
