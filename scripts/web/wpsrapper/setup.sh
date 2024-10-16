14ff7939a4857ef431551418479db07c92bc460d
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-10-16 12:17:26 +0200

    Update setup.sh

--- scripts/web/wpsrapper/setup.sh
+++ scripts/web/wpsrapper/setup.sh
@@ -1,1 +1,2 @@
 curl -s https://raw.githubusercontent.com/Vulnpire/Arsenal/refs/heads/main/scripts/web/wpsrapper/wpsrapper.sh > /tmp/wpsrapper && chmod +x /tmp/wpsrapper && sudo mv /tmp/wpsrapper /usr/local/bin/
+mkdir -p ~/.config/wpsrapper/;echo -n "\n" > ~/.config/wpsrapper/config.txt
