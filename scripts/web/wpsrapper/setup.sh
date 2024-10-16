23e45b02eac966b65236a23824c3221cbf99ea6a
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-10-16 12:11:30 +0200

    added setup script

---
+++ scripts/web/wpsrapper/setup.sh
@@ -0,0 +1,1 @@
+curl -s https://raw.githubusercontent.com/Vulnpire/Arsenal/refs/heads/main/scripts/web/wpsrapper/wpsrapper.sh > /tmp/wpsrapper && chmod +x /tmp/wpsrapper && sudo mv /tmp/wpsrapper /usr/local/bin/
