50913c54647f5f03d726a170997068ba0aaf7254
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2025-09-04 13:31:38 -0400

    Update cname_enum.sh

--- scripts/web/basic/cname_enum.sh
+++ scripts/web/basic/cname_enum.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # cname_enum.sh
-# Usage: cname_enum.sh targets.txt
+# Usage: ./cname_enum.sh targets.txt
 
 input_file="$1"
 
 while read -r target; do
-    cname=$(dig +short CNAME "$target")
+    # Skip empty lines
+    [[ -z "$target" ]] && continue
+
+    # Remove protocol (http:// or https://) if present
+    clean_target=$(echo "$target" | sed -E 's~https?://~~')
+
+    # Remove trailing slashes if any
+    clean_target=$(echo "$clean_target" | sed 's:/*$::')
+
+    cname=$(dig +short CNAME "$clean_target")
     if [[ -n "$cname" ]]; then
-        echo "$target -> $cname"
+        echo "$clean_target -> $cname"
     fi
 done < "$input_file"
